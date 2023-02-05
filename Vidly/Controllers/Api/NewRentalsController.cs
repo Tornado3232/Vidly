e504ad7752ddea2c3cac35f8ab65a6bdcebb460e
Tornado3232 <deawedaw3232@gmail.com>
2023-02-05 21:49:57 +0300

    Edge Cases Optimistic Approach.

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -33,26 +33,26 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            if(newRental.MovieIds.Count == 0)
-            {
-                return BadRequest("No Movie Ids have been giv");
-            }
+            //if(newRental.MovieIds.Count == 0)
+            //{
+            //    return BadRequest("No Movie Ids have been giv");
+            //}
 
-            var customer = _context.Customers.SingleOrDefault(
+            var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
 
-            if (customer == null)
-            {
-                return BadRequest("Customer is not valid");
-            }
+            //if (customer == null)
+            //{
+            //    return BadRequest("Customer is not valid");
+            //}
 
             var movies= _context.Movies.Where(
                 m => newRental.MovieIds.Contains(m.Id)).ToList();
 
-            if(movies.Count != newRental.MovieIds.Count)
-            {
-                return BadRequest("One or more Movie Ids are invalid.");
-            }
+            //if(movies.Count != newRental.MovieIds.Count)
+            //{
+            //    return BadRequest("One or more Movie Ids are invalid.");
+            //}
 
             foreach(var movie in movies)
             {

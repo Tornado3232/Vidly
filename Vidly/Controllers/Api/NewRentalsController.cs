c87cd01f3d9f9a379205b0d03e1a5f62d81ddb09
Tornado3232 <deawedaw3232@gmail.com>
2023-02-05 21:40:17 +0300

    Edge Cases Defensive Approach.

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -33,14 +33,35 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            var customer = _context.Customers.Single(
+            if(newRental.MovieIds.Count == 0)
+            {
+                return BadRequest("No Movie Ids have been giv");
+            }
+
+            var customer = _context.Customers.SingleOrDefault(
                 c => c.Id == newRental.CustomerId);
-            
+
+            if (customer == null)
+            {
+                return BadRequest("Customer is not valid");
+            }
+
             var movies= _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if(movies.Count != newRental.MovieIds.Count)
+            {
+                return BadRequest("One or more Movie Ids are invalid.");
+            }
 
             foreach(var movie in movies)
             {
+
+                if(movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available.");
+                }
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
